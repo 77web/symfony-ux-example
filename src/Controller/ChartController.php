faad5056148d64697d5f6c792fe2335959bc58a8
77web <info@77-web.com>
2021-01-27 23:50:37 +0900

    follow symfony/ux-chartjs doc: tryout ChartBuilder in ChartController

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -5,16 +5,50 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class ChartController extends AbstractController
 {
+    private ChartBuilderInterface $chartBuilder;
+
+    /**
+     * ChartController constructor.
+     * @param ChartBuilderInterface $chartBuilder
+     */
+    public function __construct(ChartBuilderInterface $chartBuilder)
+    {
+        $this->chartBuilder = $chartBuilder;
+    }
+
     /**
      * @Route("/chart", name="chart")
      */
     public function index(): Response
     {
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'yAxes' => [
+                    ['ticks' => ['min' => 0, 'max' => 100]],
+                ],
+            ],
+        ]);
+
         return $this->render('chart/index.html.twig', [
-            'controller_name' => 'ChartController',
+            'chart' => $chart,
         ]);
     }
 }
